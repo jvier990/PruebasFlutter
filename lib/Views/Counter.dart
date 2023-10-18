e66d0fa3f43375330fe00cbdc2e103ec51c91846
Javier <garcia.luzanilla.javier@gmail.com>
2023-10-17 17:23:56 -0700

    "Calendario & Drawbar"

--- lib/Views/Counter.dart
+++ lib/Views/Counter.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -33,6 +35,46 @@ class _practicaState extends State<practica> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple,
+      drawer: Drawer(
+          shadowColor: Colors.black54,
+          child: Container(
+            color: Colors.black,
+            child: Column(
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  margin: const EdgeInsets.only(top: 65, bottom: 20),
+                  child: Image.network(
+                      "https://media.tenor.com/ti0rtEuRNyEAAAAC/happy-halloween.gif"),
+                ),
+                const Text(
+                  "Javier Garcia",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white70),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 30),
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  color: Colors.orange[400],
+                  child: const Text("Home",
+                      style: TextStyle(color: Colors.white70)),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 2),
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  color: Colors.orange[400],
+                  child: const Text("Sign out",
+                      style: TextStyle(color: Colors.white70)),
+                ),
+              ],
+            ),
+          )),
       appBar: AppBar(
         title: const Text('Practica Fluttuer'),
         centerTitle: true,
@@ -47,21 +89,18 @@ class _practicaState extends State<practica> {
           ),
         ],
         backgroundColor: Colors.orangeAccent,
-        leading: IconButton(
-          onPressed: () {},
-          icon: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.menu),
-          ),
-        ),
       ),
-      body: //SE PUEDE CREAR UN COMPONENTE PARA EL CALENDARIO
+      body:
+
+          //SE PUEDE CREAR UN COMPONENTE PARA EL CALENDARIO
           Column(children: [
         TableCalendar(
           locale: "en_US",
           rowHeight: 45,
           headerStyle: const HeaderStyle(
-              formatButtonVisible: false, titleCentered: true),
+              decoration: BoxDecoration(color: Colors.purple),
+              formatButtonVisible: false,
+              titleCentered: true),
           availableGestures: AvailableGestures.all,
           rangeSelectionMode: RangeSelectionMode.toggledOn,
           // selectedDayPredicate: (day) => isSameDay(day, today),
@@ -72,7 +111,12 @@ class _practicaState extends State<practica> {
           rangeStartDay: _RangeStart,
           rangeEndDay: _RangeFinish,
           onRangeSelected: _onDaysSelected,
-        )
+          calendarStyle: CalendarStyle(
+              defaultDecoration: BoxDecoration(color: Colors.orange),
+              outsideDecoration: BoxDecoration(color: Colors.orange),
+              holidayDecoration: BoxDecoration(color: Colors.orange),
+              weekendDecoration: BoxDecoration(color: Colors.orange)),
+        ),
       ]),
     );
   }
